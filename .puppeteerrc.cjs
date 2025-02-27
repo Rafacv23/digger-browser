5caabcbe8b1ff3fe4dadfb849931adec1862a286
Rafa Canosa <rafacv23@gmail.com>
2025-02-27 23:35:18 +0100

    Add Puppeteer configuration and update postinstall script

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,17 @@
+import { join } from "path"
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  // Changes the cache location for Puppeteer.
+  cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+
+  chrome: {
+    skipDownload: false,
+  },
+  // Download Firefox (default `skipDownload: true`).
+  firefox: {
+    skipDownload: false,
+  },
+}
