5375db39106443470eb3109d8c85ae54640364c4
Rafa <rafacv23@gmail.com>
2025-02-27 11:16:54 +0100

    Add Footer component with GitHub and Portfolio links

---
+++ src/components/Footer.tsx
@@ -0,0 +1,26 @@
+import { Github, PersonStanding } from "lucide-react"
+
+export default function Footer() {
+  return (
+    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Github size={16} />
+        GitHub
+      </a>
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <PersonStanding size={16} />
+        Porrfolio
+      </a>
+    </footer>
+  )
+}
