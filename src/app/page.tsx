7a404c7a7932f7f066431a91f3b22eda8ec9d383
Rafa <rafacv23@gmail.com>
2025-03-01 12:10:46 +0100

    Implement search functionality with input validation and display results

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,40 @@
+"use client"
+
 import Footer from "@/components/Footer"
 import { Search } from "lucide-react"
+import { useState } from "react"
 
 export default function Home() {
+  const [data, setData] = useState("")
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+
+    const query = formData.get("q")
+
+    if (!query) {
+      return alert("Please enter a search query.")
+    }
+
+    const res = await fetch(`/api/search?q=${query}`)
+
+    const data = await res.json()
+
+    setData(JSON.stringify(data, null, 2))
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-5xl font-bold">
           Welcome to <span className="text-primary">Digger</span>
         </h1>
-        <form action="/api/search" method="get" className="flex gap-4 flex-col">
+        <form
+          onSubmit={handleSubmit}
+          method="get"
+          className="flex gap-4 flex-col"
+        >
           <div className="flex gap-4">
             <input
               type="text"
@@ -28,6 +54,9 @@ export default function Home() {
             </button>
           </div>
         </form>
+        <pre className="max-w-sm overflow-auto">
+          <code>{data}</code>
+        </pre>
       </main>
       <Footer />
     </div>
