5274110396e4739cf1ecd509302e46f0efa24904
Rafa <rafacv23@gmail.com>
2025-03-01 13:09:02 +0100

    Refactor search functionality and add menu components for improved user experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
 "use client"
 
 import Footer from "@/components/Footer"
-import { Search } from "lucide-react"
+import { PreSearchMenu, ResultsMenu } from "@/components/Menu"
+import { ApiResponse } from "@/types/types"
+import { LoaderCircle, Search } from "lucide-react"
 import { useState } from "react"
 
 export default function Home() {
-  const [data, setData] = useState("")
+  const [data, setData] = useState<ApiResponse | null>(null)
+  const [showMenu, setShowMenu] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(false)
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+
+    setLoading(true)
+
     const formData = new FormData(event.currentTarget)
 
     const query = formData.get("q")
@@ -17,15 +24,20 @@ export default function Home() {
       return alert("Please enter a search query.")
     }
 
+    // save the query to local storage
+    localStorage.setItem("query", query.toString())
+
     const res = await fetch(`/api/search?q=${query}`)
 
     const data = await res.json()
 
-    setData(JSON.stringify(data, null, 2))
+    setData(data)
+
+    setLoading(false)
   }
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-literata)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-5xl font-bold">
           Welcome to <span className="text-primary">Digger</span>
@@ -36,27 +48,38 @@ export default function Home() {
           className="flex gap-4 flex-col"
         >
           <div className="flex gap-4">
+            <label htmlFor="q" className="sr-only">
+              Search query
+            </label>
             <input
-              type="text"
+              type="search"
+              id="q"
               name="q"
               placeholder="Search..."
               required
               min={2}
               max={60}
               className="px-4 py-2 border border-gray-300 rounded-md text-black"
+              onClick={() => setShowMenu(true)}
             />
             <button
               type="submit"
               className="flex items-center gap-2 bg-white px-4 py-2 rounded-md text-black"
+              disabled={loading}
             >
-              <Search size={16} />
-              Search
+              {loading ? (
+                <LoaderCircle className="animate-spin" size={16} />
+              ) : (
+                <Search size={16} />
+              )}
             </button>
           </div>
+          {data ? (
+            <ResultsMenu data={data} />
+          ) : showMenu ? (
+            <PreSearchMenu />
+          ) : null}
         </form>
-        <pre className="max-w-sm overflow-auto">
-          <code>{data}</code>
-        </pre>
       </main>
       <Footer />
     </div>
---
+++ src/components/Menu.tsx
@@ -0,0 +1,46 @@
+import { ApiResponse } from "@/types/types"
+import Link from "next/link"
+
+export function ResultsMenu({ data }: { data: ApiResponse }) {
+  return (
+    <div>
+      <h2>Query {data.query}</h2>
+      <h2>Pages</h2>
+      <ul>
+        {data.pages.map((page) => (
+          <li key={page.link}>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href={page.link}
+            ></Link>
+          </li>
+        ))}
+      </ul>
+      <h2>Results</h2>
+      <article>{data.resume}</article>
+    </div>
+  )
+}
+
+// menu for recent searches, displays before the user have results
+export function PreSearchMenu() {
+  return (
+    <div>
+      <h2>Recent</h2>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </ul>
+    </div>
+  )
+}
+
+// Displays the controlls for the menu
+export function FooterMenu() {
+  return (
+    <footer>
+      <h2>Controlls</h2>
+    </footer>
+  )
+}
