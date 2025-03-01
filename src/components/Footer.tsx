fd5542a0b93b40e7bc005b0e95dcc1000751a388
Rafa <rafacv23@gmail.com>
2025-03-01 13:35:22 +0100

    Refactor Footer component to use Link for navigation and apply consistent styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,18 @@
 import { GITHUB_REPO, PORTFOLIO } from "@/lib/constants"
 import { Github, PersonStanding } from "lucide-react"
+import Link from "next/link"
 
 export default function Footer() {
   return (
     <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href={GITHUB_REPO}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <Link href={GITHUB_REPO} target="_blank" rel="noopener noreferrer">
         <Github size={16} />
         GitHub
-      </a>
-      <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href={PORTFOLIO}
-        target="_blank"
-      >
+      </Link>
+      <Link href={PORTFOLIO} target="_blank">
         <PersonStanding size={16} />
         Portfolio
-      </a>
+      </Link>
     </footer>
   )
 }
