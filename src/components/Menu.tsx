60a07887b675a5332f28c8d2edc60bdec8846369
Rafa <rafacv23@gmail.com>
2025-03-03 13:29:16 +0100

    Enhance ResultsMenu and PreSearchMenu with improved styling and external link icons

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,13 +1,14 @@
 import { getRecentSearchs } from "@/lib/utils"
 import { ApiResponse } from "@/types/types"
+import { ExternalLink } from "lucide-react"
 import Link from "next/link"
 
 export function ResultsMenu({ data }: { data: ApiResponse }) {
   return (
     <div>
-      <h2>Query {data.query}</h2>
+      <h2>{data.query}</h2>
       <h2>Pages</h2>
-      <ul>
+      <ul className="mb-4">
         {data.pages.map((page) => (
           <Link
             key={page.link}
@@ -16,6 +17,7 @@ export function ResultsMenu({ data }: { data: ApiResponse }) {
             href={page.link}
           >
             {page.title}
+            <ExternalLink size={16} />
           </Link>
         ))}
       </ul>
@@ -39,9 +41,14 @@ export function PreSearchMenu() {
   return (
     <div>
       <h2>Recent</h2>
-      <ul>
+      <ul className="flex flex-wrap gap-2 mb-4">
         {recentSearchs.map((search: ApiResponse) => (
-          <li key={search.query}>{search.query}</li>
+          <li
+            key={search.query}
+            className="border border-primary shadow rounded-md px-4 py-2 font-light"
+          >
+            {search.query}
+          </li>
         ))}
       </ul>
       <h2>Related</h2>
@@ -50,13 +57,17 @@ export function PreSearchMenu() {
           <li key={pages[0].title}>
             <ul>
               {pages.map((page) => (
-                <li key={page.title}>
+                <li
+                  key={page.title}
+                  className="flex items-center justify-between"
+                >
                   <Link
                     target="_blank"
                     rel="noopener noreferrer"
                     href={page.link}
                   >
                     {page.title}
+                    <ExternalLink size={16} />
                   </Link>
                 </li>
               ))}
