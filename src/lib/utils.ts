756426d98c6dde34223e6cdeccfbc479b1807ed2
Rafa <rafacv23@gmail.com>
2025-03-03 14:12:32 +0100

    Add retrieveDomain utility function to extract domain from a URL

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -32,3 +32,9 @@ export function getRecentSearchs() {
 
   return existingArray
 }
+
+// this function receives a url and returns the domain of that url
+export function retrieveDomain(url: string): string {
+  const domain = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i)
+  return domain ? domain[2] : ""
+}
