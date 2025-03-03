14a6b5b555a2750f0599e418ac41985e4fb07711
Rafa <rafacv23@gmail.com>
2025-03-03 13:29:11 +0100

    Add Header component for site navigation and branding

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between py-4 mx-auto text-primary">
+      <div>
+        <Link href="/" title="Home">
+          Digger Browser
+        </Link>
+      </div>
+      <div className="flex items-center gap-4">
+        <Link href="/about" title="About">
+          About
+        </Link>
+        <Link href="/donate" title="Donate">
+          Donate
+        </Link>
+      </div>
+    </header>
+  )
+}
