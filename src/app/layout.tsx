971b469757a90ed8a3781c669bc65869708f48e2
Rafa <rafacv23@gmail.com>
2025-03-10 12:49:06 +0100

    Enhance metadata for improved SEO and social sharing capabilities

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,11 @@ import { Literata } from "next/font/google"
 import "./globals.css"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
+import {
+  SITE_META_DESCRIPTION,
+  SITE_META_NAME,
+  SITE_URL,
+} from "@/lib/constants"
 
 const literata = Literata({
   variable: "--font-literata",
@@ -13,6 +18,74 @@ export const metadata: Metadata = {
   title: "Digger Browser",
   description:
     "Next generation AI powered browser, that scraps the resuts of the web for you.",
+  creator: "Rafa Canosa",
+  authors: { name: "Rafa Canosa" },
+  metadataBase: new URL(SITE_URL),
+  category: "search engine",
+  verification: { google: "8Ssu9wyMqGLnOHp01rAbY2QJAKTU4XkqTq9OPShpeOc" },
+  openGraph: {
+    type: "website",
+    url: SITE_URL,
+    title: SITE_META_NAME,
+    description: SITE_META_DESCRIPTION,
+    siteName: SITE_META_NAME,
+    images: [
+      {
+        url: "/favicon3.png",
+        width: 1200,
+        height: 630,
+        alt: SITE_META_NAME,
+      },
+    ],
+  },
+  alternates: {
+    canonical: "/",
+  },
+  keywords: [
+    "ai",
+    "browser",
+    "next generation",
+    "search",
+    "scraping",
+    "web",
+    "digger",
+    "digger browser",
+    "digger-browser",
+    "next-generation-ai",
+    "next-generation-browser",
+    "next-generation-search",
+    "next-generation-scraping",
+    "next-generation-web",
+    "next-gen-ai",
+    "next-gen-browser",
+    "next-gen-search",
+  ],
+  referrer: "origin-when-cross-origin",
+  icons: {
+    icon: "/favicon3.ico",
+    apple: "/favicon3.ico",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@rafacanosa",
+    creator: "@rafacanosa",
+    title: SITE_META_NAME,
+    description: SITE_META_DESCRIPTION,
+    images: [
+      {
+        url: "/favicon3.png",
+        alt: SITE_META_NAME,
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,7 @@
 export const GITHUB_REPO = "https://github.com/rafacv23/digger-browser"
 export const PORTFOLIO = "https://www.rafacanosa.dev"
+export const SITE_URL = "https://digger-browser.vercel.app"
+export const SITE_NAME = "Digger Browser"
+export const SITE_META_NAME = "Digger Browser"
+export const SITE_META_DESCRIPTION =
+  "Next generation AI powered browser, that scraps the resuts of the web for you."
