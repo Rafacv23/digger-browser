b8fe5a9789f2d8e064c55bbb0bff84e95a421fdb
Rafa <rafacv23@gmail.com>
2025-03-10 12:42:20 +0100

    Update sitemap priorities and add download page entry

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -12,13 +12,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: "https://digger-browser.vercel.app/about",
       lastModified: new Date(),
       changeFrequency: "monthly",
-      priority: 0.8,
+      priority: 0.5,
     },
     {
       url: "https://digger-browser.vercel.app/donate",
       lastModified: new Date(),
       changeFrequency: "weekly",
-      priority: 0.5,
+      priority: 0.6,
+    },
+    {
+      url: "https://digger-browser.vercel.app/download",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
   ]
 }
